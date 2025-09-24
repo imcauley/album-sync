901e331b12602f94da0112a68b0da073bd1a6be4
Isaac McAuley <mcauleyisaac@gmail.com>
2025-09-24 17:08:09 -0400

    Shows selected directory

---
+++ README.md
@@ -0,0 +1,8 @@
+## Milestones
+
+- [x] Select directory of albums
+- [x] Show selected directories contents
+- [ ] Can scroll list
+- [ ] Select specific subdirectories to add
+- [ ] Copy contents to other directory
+- [ ] Show size of directory
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::{
+    fs::{self, ReadDir},
+    path::PathBuf,
+};
+
 use iced::widget::{button, column, text};
 use rfd::FileDialog;
 
@@ -13,27 +18,46 @@ enum Message {
 #[derive(Default)]
 struct MyApp {
     counter: usize,
-    source_folder: String,
+    error_message: String,
+    source_folder: Option<PathBuf>,
+    source_albums: Vec<String>,
 }
 
 impl MyApp {
     fn update(&mut self, message: Message) {
         match message {
-            Message::ButtonPressed => {
-                let folder_handler = FileDialog::new().set_directory("~").pick_folder();
-                match folder_handler {
-                    None => self.source_folder = "".to_string(),
-                    Some(f) => self.source_folder = f.display().to_string(),
-                }
-            }
+            Message::ButtonPressed => self.load_albums(),
         }
     }
 
+    fn load_albums(&mut self) {
+        self.source_folder = FileDialog::new().set_directory("~").pick_folder();
+
+        match &self.source_folder {
+            None => {}
+            Some(f) => match fs::read_dir(f.display().to_string()) {
+                Err(_) => self.error_message = "Could not load directory".to_string(),
+                Ok(r) => {
+                    self.source_albums = r
+                        .filter_map(|entry| entry.ok())
+                        .filter_map(|entry| {
+                            entry.file_name().into_string().ok() // Skip if the filename is not valid UTF-8
+                        })
+                        .collect()
+                }
+            },
+        };
+    }
+
     fn view(&self) -> iced::Element<Message> {
-        column![
-            text(self.source_folder.clone()),
-            button("Increase").on_press(Message::ButtonPressed),
-        ]
-        .into()
+        let test = self
+            .source_albums
+            .iter()
+            .map(|e| iced::widget::Text::new(e).into());
+
+        let mut window = iced::widget::Column::with_children(test);
+        window = window.push(iced::widget::Text::new("!"));
+        window = window.push(button("Increase").on_press(Message::ButtonPressed));
+        return window.into();
     }
 }
